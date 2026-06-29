7c9f299b7425ecc90a2618f561786f379c59a567
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-06-30 02:04:42 +0530

    feat: initialize socket connection in server setup

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import http from "http";
 import app from "./app.js";
 import connectDB from "./shared/config/db.config.js";
+import initializeSocket from "./sockets/index.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +12,10 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    const server = http.createServer(app);
+    const server =
+  http.createServer(app);
+
+initializeSocket(server);
 
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
