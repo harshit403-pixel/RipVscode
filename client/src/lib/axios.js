2da5a7acee8f93d0fcf865b69103253ac5fb6600
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-06-30 17:59:51 +0530

    feat: add axios interceptor for authorization token and configure Redux store with auth reducer

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import store from "./store";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000/api",
@@ -7,5 +8,21 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token =
+      store.getState().auth.user?.accessToken;
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
+
+
