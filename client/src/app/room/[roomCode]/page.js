b876fcda4fb12fe90be358068b0a5d939848d511
Bhavya Dhanwani <147585292+Bhavya-Dhanwani@users.noreply.github.com>
2026-06-30 20:46:23 +0530

    Update page.js

--- client/src/app/room/[roomCode]/page.js
+++ client/src/app/room/[roomCode]/page.js
@@ -3,7 +3,7 @@
 import { useParams } from "next/navigation";
 import RoomPage from "@/features/room/ui/jsx/RoomPage";
 
-export default function RoomPage() {
+export default function Page() {
   const { roomCode } = useParams();
   const router = useRouter();
 
